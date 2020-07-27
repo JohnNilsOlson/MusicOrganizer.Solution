a10726447847390d2ba1d2b17faed98ff97304aa
JohnNils Olson <johnnils@gmail.com>
2020-07-27 10:37:10 -0700

    add test for getall database method
    Co-Authored-By: Cody Fritz <clanalia55@gmail.com>

--- MusicOrganizerTests/ModelTests/RecordTests.cs
+++ MusicOrganizerTests/ModelTests/RecordTests.cs
@@ -6,16 +6,24 @@ using MySql.Data.MySqlClient;
 
 namespace MusicOrganizer.Tests
 {
-[TestClass]
+  [TestClass]
   public class RecordTest : IDisposable
   {
     public void Dispose()
     {
-      RecordTest.ClearAll();
+      Record.ClearAll();
     }
     public RecordTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=music_organizer_test;";
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyListFromDatabase_RecordList()
+    {
+      List<Record> newList = new List<Record> { };
+      List<Record> result = Record.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
